87e79036007b750b77a93144bf229141a8c058c7
oussamaa koz <oussamaakalay76@gmail.com>
2022-02-01 00:57:34 -0800

    update

---
+++ src/js/weatherManager.js
@@ -0,0 +1,39 @@
+class WeatherManager {
+    #_currentForecast;
+    #_weekForecast;
+    #_errorMsg = undefined;
+
+    set currentForecast(value) {
+        this.#_currentForecast = value
+    }
+
+    get currentForecast() {
+        return this.#_currentForecast
+    }
+
+    get errorMsg() {
+        return this.#_errorMsg;
+    }
+
+    async fetchForecast(url) {
+
+        var response = await fetch(url)
+        var json = await response.json()
+        // handle the response
+
+        var weather = new Weather()
+   
+
+        if(!response.ok){
+            this.#_errorMsg = `An error has occured: ${response.status}`
+        } else {
+            this.#_errorMsg = undefined
+            weather.city = json.name
+            weather.description = json.weather[0].main
+            weather.iconCode = json.weather[0].icon
+            weather.temp = json.main.temp
+            weather.humidity = json.main.humidity
+            this.#_currentForecast = weather
+        }
+    }
+}
